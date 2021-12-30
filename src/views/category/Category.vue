ac0b1192c8ebe6f6a509b3a014ff01f29096bbef
zxzxhy <1914203102@qq.com>
2021-12-30 20:51:33 +0800

    初始化项目

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,67 @@
 <template>
-  <div>分类页面</div>
+  <div class="category">
+    <nav-bar class="category-navbar">
+      <template #center>
+        <div>商品分类</div>
+      </template>
+    </nav-bar>
+    <div class="content">
+      <tab-menu :categories="categories" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import NavBar from "components/common/navbar/NavBar.vue";
+import TabMenu from "./childComps/TabMenu.vue";
 
-}
+import { getCategory, getSubcategory } from "network/category";
+
+export default {
+  components: {
+    NavBar,
+    TabMenu,
+  },
+  data() {
+    return {
+      categories: [],
+      categoryData:{},
+      currentIndex: -1,
+    };
+  },
+  created() {
+    // 请求分类数据
+    this.getCategoryList();
+  },
+  methods: {
+    getCategoryList() {
+      getCategory().then((res) => {
+        this.categories = res.data.category.list;
+      });
+    },
+    getSubcategoryInfo(index) {
+      this.currentIndex = index
+      const mailKey = this.categories[index].mailKey
+      getSubcategory(mailKey).then(res =>{
+        this.categoryData[index] = res.data
+         this.categoryData = [... this.categoryData]
+      })
+    },
+  },
+};
 </script>
 
 <style>
-
+.category {
+  height: 100vh;
+}
+.category-navbar {
+  position: relative;
+  left: 0;
+  top: 0;
+  color: #fff;
+  font-weight: bolder;
+  background-color: var(--color-tint);
+  z-index: 10;
+}
 </style>
---
+++ src/views/category/childComps/TabMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <scroll class="tab-menu ">
+    <div class="menu-list">
+      <div
+        class="menu-list-item"
+        v-for="(item, index) in categories"
+        :key="index"
+        @click="itemClick(index)"
+        :class="{ active: index === currentIndex }"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+  </scroll>
+</template>
+
+<script>
+import Scroll from 'components/common/scroll/Scroll.vue'
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  components:{Scroll},
+  props: {
+    categories: {
+      type: Array,
+    },
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
+</script>
+
+<style>
+.tab-menu {
+  width: 100px;
+  height: calc(100vh - 49px - 44px);
+  background-color: #f6f6f6;
+  box-sizing: border-box;
+}
+.menu-list-item {
+  height: 45px;
+  line-height: 45px;
+  text-align: center;
+  font-size: 14px;
+}
+.menu-list-item.active {
+  font-weight: 700;
+  color: var(--color-high-text);
+  background-color: #fff;
+  border-left: 3px solid var(--color-high-text);
+}
+</style>
